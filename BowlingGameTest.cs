62556c317fc52e4ae3815f4893974a5723635513
FranksBrain <frank@franksbrain.com>
2016-11-23 13:55:18 -0500

    Initial commit

---
+++ BowlingGameTest.cs
@@ -0,0 +1,96 @@
+﻿using Xunit;
+
+namespace BowlingKata
+{
+    public class BowlingGameTest
+    {
+        private Game g;
+
+        public BowlingGameTest()
+        {
+            g = new Game();
+        }
+
+        private void rollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+
+        private void rollSpare()
+        {
+            g.Roll(6);
+            g.Roll(4);
+        }
+
+        [Fact]
+        public void TestBowlingGameClass()
+        {
+            Assert.IsType<Game>(g);
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            rollMany(20, 0);
+            Assert.Equal(0, g.Score());
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            rollMany(20, 1);
+            Assert.Equal(20, g.Score());
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            rollSpare();
+            g.Roll(4);
+            rollMany(17, 0);
+            Assert.Equal(18, g.Score());
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            g.Roll(10);
+            g.Roll(4);
+            g.Roll(5);
+            rollMany(17, 0);
+            Assert.Equal(28, g.Score());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            rollMany(12, 10);
+            Assert.Equal(300, g.Score());
+        }
+
+        [Fact]
+        public void TestRandomGameNoExtraRoll()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 0 });
+            Assert.Equal(131, g.Score());
+        }
+
+        [Fact]
+        public void TestRandomGameWithSpareThenStrikeAtEnd()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 1, 10 });
+            Assert.Equal(143, g.Score());
+        }
+
+        [Fact]
+        public void TestRandomGameWithThreeStrikesAtEnd()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 10, 10, 10 });
+            Assert.Equal(163, g.Score());
+        }
+
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,58 @@
+﻿namespace BowlingKata
+{
+    internal class Game
+    {
+        //private int score = 0;
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        private bool isSpare(int frameIndex) {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for(int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+                
+            }
+            
+            return score;
+        }
+    }
+}
